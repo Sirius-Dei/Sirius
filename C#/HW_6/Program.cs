f290cb2faf16a86b05fc08f6cdb2b973f66d37fc
Sirius <siriusdei@mail.ru>
2022-12-18 13:20:42 +0300

    HW_6 Complete

---
+++ C#/HW_6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter the number");
+int n = int.Parse(Console.ReadLine());
+int r = (n % 2);
+if (r == 0) System.Console.WriteLine($"Number {n} is even!");
+else System.Console.WriteLine($"Number {n} is odd!");
