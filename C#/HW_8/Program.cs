8947f0c688990dbbe1c6349e452d301854d1f0d2
Sirius <siriusdei@mail.ru>
2022-12-18 16:12:31 +0300

    HW_8 Complete

---
+++ C#/HW_8/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] arg)
+{
+    int length = arg.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arg[index] = index + 1;
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+System.Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int[] arr = new int[n];
+FillArray(arr);
+
+
+int r = (n % 2);
+int size = arr.Length;
+for (int i = 1; i < size; i++)
+    if (r == 0) //Если чётное то
+    {
+        System.Console.Write($"{arr[i]}, ");
+        i++;
+    }
+    else if (r != 0) // Если не чётное
+    {
+        Console.Write($"{arr[i]}, ");
+        i++;
+    }
+
+
+
+
