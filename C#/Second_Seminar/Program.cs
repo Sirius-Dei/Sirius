a483b5ffaa08811403ae3a06378d690764eac071
Sirius <siriusdei@mail.ru>
2022-12-18 18:46:39 +0300

    Seminar 2 end

--- C#/Second_Seminar/Program.cs
+++ C#/Second_Seminar/Program.cs
@@ -8,7 +8,52 @@
 
 int getNumberFromUser()
 {
+    System.Console.WriteLine("Введи трёхзначное число от 100 до 999");
     int temp = int.Parse(Console.ReadLine());
 
     return temp;
 }
+
+bool validator(int number)
+{
+    return number >= 100 && number <= 999;
+    //    if (number >= 100)
+    //   {
+    //        if (number <= 999)
+    //        {
+    //            return true;
+    //        }
+    //    }
+    //    return false;
+}
+
+string GetText(int input, int output)
+{
+    return input + "->" + output;
+}
+
+string Decorator(string text)
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Red;
+    return text;
+}
+
+void Task36()
+{
+    int value = getNumberFromUser();
+    if (validator(value))
+    {
+        int result = GetNumber(value);
+        System.Console.WriteLine(Decorator(GetText(value, result)));
+    }
+    else
+    {
+        System.Console.WriteLine("WRONG NUMBER!");
+    }
+}
+
+
+
+
+
