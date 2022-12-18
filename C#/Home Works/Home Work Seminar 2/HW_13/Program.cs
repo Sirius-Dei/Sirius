aff1db10330642a683e15ef2b906c66f74eb08a3
Sirius <siriusdei@mail.ru>
2022-12-18 20:48:04 +0300

    HW_15 Complete

---
+++ C#/Home Works/Home Work Seminar 2/HW_13/Program.cs
@@ -0,0 +1,24 @@
+﻿//Входные данные получение
+int GetValue()
+{
+    System.Console.WriteLine("Введите число");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+
+}
+
+int n = GetValue();
+
+
+
+
+
+
+
+
+int[] arr = new int[n];
+int size = arr.Length;
+int index = 0;
+while (index < size)
+    int thirdNum
+index++;
---
+++ C#/Home Works/Home Work Seminar 2/HW_15/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.WriteLine("Введите число от 1 до 7");
+int i = int.Parse(Console.ReadLine());
+switch (i)
+{
+    case 1: System.Console.Write("Понедельник"); break;
+    case 2: System.Console.Write("Вторник"); break;
+    case 3: System.Console.Write("Среда"); break;
+    case 4: System.Console.Write("Четверг"); break;
+    case 5: System.Console.Write("Пятница"); break;
+    case 6: System.Console.Write("Суббота"); break;
+    case 7: System.Console.Write("Воскресенье"); break;
+    default: System.Console.WriteLine("Это не день недели!"); break;
+}
+if (i == 6)
+{
+    System.Console.WriteLine(", наконец то выходной!");
+}
+else if (i == 7)
+{
+    System.Console.WriteLine(", хоть и выходной, но завтра на работу :(");
+}
