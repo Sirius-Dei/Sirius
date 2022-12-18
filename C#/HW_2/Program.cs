e094191a3e4f6c18c4faf2128e3d316b9552d83f
Sirius <siriusdei@mail.ru>
2022-12-18 12:13:16 +0300

    HW_2 Complete

---
+++ C#/HW_2/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Enter first number ");
+int a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter second number ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    System.Console.WriteLine($"{a} больше {b}");
+}
+else
+{
+    System.Console.WriteLine($"{b} больше {a}");
+}
