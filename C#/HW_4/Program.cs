21375ec9335acc45f843c20652181e899272353c
Sirius <siriusdei@mail.ru>
2022-12-18 12:41:32 +0300

    HW_4 Complete

---
+++ C#/HW_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine($"Maximal = {max}");
