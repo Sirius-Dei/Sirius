d1d4a92d9ea9647b9e8749330ac06efedd272421
Sirius <siriusdei@mail.ru>
2022-12-18 19:26:47 +0300

    HW_10 Complete

---
+++ C#/Home Works/Home Work Seminar 2/HW_10/Program.cs
@@ -0,0 +1,33 @@
+﻿//Ввод данных 
+int Enter()
+{
+    System.Console.WriteLine("Введи трёхзначное число от 100 до 999");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+//Метод получения второй цифры числа
+int GetSecondNumber(int value)
+{
+    int a = value / 100;
+    int b = (value / 10) % 10;
+    int c = value % 10;
+    return b;
+}
+
+//Метод отсеивания неправильных чисел
+bool valid(int validNums)
+{
+    return validNums >= 100 && validNums <= 999;
+}
+
+int value = Enter();
+if (valid(value))
+{
+    int result = GetSecondNumber(value);
+    System.Console.WriteLine($"Вторая цифра числа {value} -> {result} !");
+}
+else
+{
+    System.Console.WriteLine("Введите корректное число!");
+}
--- C#/Second_Seminar/Program.cs
+++ C#/Second_Seminar/Program.cs
@@ -52,8 +52,3 @@ void Task36()
         System.Console.WriteLine("WRONG NUMBER!");
     }
 }
-
-
-
-
-
