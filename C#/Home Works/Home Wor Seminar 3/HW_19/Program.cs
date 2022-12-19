b68318f0290da10debef2cc5d1af067300f587b1
Sirius <siriusdei@mail.ru>
2022-12-19 22:20:14 +0300

    HW_19 Complete

---
+++ C#/Home Works/Home Wor Seminar 3/HW_19/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Введите пятизначное число");
+string n = Console.ReadLine();
+if (n.Length == 5)
+{
+    if (n[0] == n[4] && n[1] == n[3])
+    {
+        System.Console.WriteLine($"Число '{n}' является палиндромом!");
+
+    }
+    else System.Console.WriteLine($"Число '{n}' НЕ является палиндромом!");
+}
+else System.Console.WriteLine("Неправильное число");
