9a6ab30b90b2dd93e02bc38adcc0d4abdd7eb43b
Sirius <siriusdei@mail.ru>
2022-12-20 13:44:22 +0300

    All HW Sem 3 Cpmplete!

--- C#/Home Works/Home Work Seminar 3/HW_23/Program.cs
+++ C#/Home Works/Home Work Seminar 3/HW_23/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿System.Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+while (count <= N)
+{
+    Console.Write(Math.Pow(count, 3));
+    count++;
+    System.Console.Write(", ");
+}
