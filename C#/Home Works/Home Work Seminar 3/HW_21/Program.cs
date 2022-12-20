9928aad01ac5fd4fba4f13da52b9fb08d46e2a89
Sirius <siriusdei@mail.ru>
2022-12-20 13:17:04 +0300

    HW_21 Complete

---
+++ C#/Home Works/Home Work Seminar 3/HW_21/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите X для А");
+int ax = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите Y для А");
+int ay = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите Z для А");
+int az = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите X для B");
+int bx = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите Y для B");
+int by = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите Z для B");
+int bz = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bz - az, 2));
+System.Console.WriteLine("Расстояние между точками 'A,B' в 3D пространстве = " + Math.Round(d, 2));
---
+++ C#/Third Seminar/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Console.WriteLine("Введите номер четверти");
+int qwarter = int.Parse(Console.ReadLine());
+if (qwarter >= 1 && qwarter <= 4)
+{
+    if (qwarter == 1)
+    {
+        System.Console.WriteLine($"X (1, {int.MaxValue}), Y (1, {int.MaxValue})");
+    }
+    if (qwarter == 2)
+    {
+        System.Console.WriteLine($"X (-1, {int.MinValue}), Y (1, {int.MaxValue})");
+    }
+    if (qwarter == 3)
+    {
+        System.Console.WriteLine($"X (-1, {int.MinValue}),Y (1, {int.MinValue})");
+    }
+    if (qwarter == 4)
+    {
+        System.Console.WriteLine($"X (1, {int.MaxValue}), Y (-1, {int.MinValue})");
+    }
+}
+else System.Console.WriteLine("Неправильнный номер четверти!");
