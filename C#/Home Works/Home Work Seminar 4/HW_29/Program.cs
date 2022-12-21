772623b17048b4b17296d3ef09f9f85cb4881673
Sirius <siriusdei@mail.ru>
2022-12-21 17:12:15 +0300

    HW_29 Complete

---
+++ C#/Home Works/Home Work Seminar 4/HW_29/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt("Number");
+    }
+    return array;
+
+}
+string ArrayToString(int[] array)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i]}, ";
+    }
+    return result;
+}
+int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] arr = GetArray(8);
+string res = ArrayToString(arr);
+System.Console.Write(res);
