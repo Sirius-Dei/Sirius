18f069e2fe99febed31863a1d01e96b8468431d5
Sirius <siriusdei@mail.ru>
2022-12-21 18:02:23 +0300

    HW_25 Complete

---
+++ C#/Home Works/Home Work Seminar 4/HW_25/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+double pow(int numA, int numB)
+{
+    double res = Math.Pow(numA, numB);
+    return res;
+}
+
+
+System.Console.WriteLine("Введите число А");
+int A = ReadInt("Num A");
+System.Console.WriteLine("Введите число B");
+int B = ReadInt("Num B");
+System.Console.WriteLine(pow(A, B));
+
---
+++ C#/Home Works/Home Work Seminar 4/HW_27/Program.cs
@@ -0,0 +1,15 @@
+﻿int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+
+System.Console.WriteLine("Input number");
+string n = ReadInt("Number").ToString();
+int size = n.Length;
+int sum = 0;
+for (int i = 0; i < size; i++)
+{
+    sum = n[0] + n[1];
+}
+System.Console.WriteLine(sum);
