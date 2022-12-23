e30019ff3d9a51417abb8b2581fd7c9f01600385
Sirius <siriusdei@mail.ru>
2022-12-23 11:20:38 +0300

    add gitignore

---
+++ C#/Home Works/Home Work Seminar 5/HW_36/Program.cs
@@ -0,0 +1,17 @@
+﻿int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+int[] GetArray(int size)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt("Number");
+    }
+    return array;
+}
+
+
