c547ca64247641a160eaa0436f44e192684d29c3
Sirius <siriusdei@mail.ru>
2022-12-23 14:49:31 +0300

    HW_34 Complete

---
+++ C#/Home Works/Home Work Seminar 5/HW_34/Program.cs
@@ -0,0 +1,49 @@
+﻿int ReadInt(string argumentName)
+{
+    Console.Write($"Input {argumentName}: ");
+    return int.Parse(Console.ReadLine()!);
+}
+int[] GetRandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+}
+
+
+int length = ReadInt("Length");
+int[] array = GetRandomArray(length, 100, 1000);
+PrintArray(array);
+System.Console.Write("- Массив");
+System.Console.WriteLine();
+System.Console.Write(even(array));
+System.Console.Write(" - Колличество чётных чисел в массиве");
+
+
+
+
+int even(int[] nums)
+{
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (nums[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
+
+
--- C#/Home Works/Home Work Seminar 5/HW_36/Program.cs
+++ C#/Home Works/Home Work Seminar 5/HW_36/Program.cs
@@ -1,17 +1,1 @@
-﻿int ReadInt(string argumentName)
-{
-    Console.Write($"Input {argumentName}: ");
-    return int.Parse(Console.ReadLine()!);
-}
-int[] GetArray(int size)
-{
-    int[] array = new int[size];
-
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = ReadInt("Number");
-    }
-    return array;
-}
-
-
+﻿
