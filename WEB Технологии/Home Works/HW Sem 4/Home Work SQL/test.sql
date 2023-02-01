fb7919fe6dcac448f4d4fb7e7b85f3c71689b6bb
Sirius <siriusdei@mail.ru>
2023-02-01 18:13:19 +0300

    HW Sem 4 Complete

---
+++ WEB Технологии/Home Works/HW Sem 4/Home Work SQL/test.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM sirius.classmates
+LIMIT 0, 1000
+
+-- Date: 2023-02-01 17:00
+*/
+INSERT INTO `` (`id`,`name`,`age`,`adress`) VALUES (1,'Иван','18','Россия');
+INSERT INTO `` (`id`,`name`,`age`,`adress`) VALUES (2,'Пётр','22','Индонезия');
+INSERT INTO `` (`id`,`name`,`age`,`adress`) VALUES (3,'Анна','35','Германия');
+INSERT INTO `` (`id`,`name`,`age`,`adress`) VALUES (4,'Юлия','18','Польша');
+INSERT INTO `` (`id`,`name`,`age`,`adress`) VALUES (5,'Николай','36','Грузия');
---
+++ WEB Технологии/Test HTML/script.js
@@ -0,0 +1,28 @@
+// function GetName(){
+//     alert("Привет! Давай познакомимся!")
+//     let firstName = prompt("Как тебя зовут?");
+//     while(firstName==false) firstName = prompt("Грубо не говорить своё имя");
+//     alert("Спасибо")
+//     let secondName = prompt("Какая у тебя фамилия?");
+//     while(secondName==false) secondName = prompt("Давай не стесняйся");
+//     alert(`Привет ${firstName} ${secondName} !`)
+// }
+// GetName();
+
+// function FavFilm(){
+//     count=0;
+//     while(count==0){
+//         let FavFilm = prompt("Расскажи как называется твой любимый фильм");
+//         switch(FavFilm){
+//             case "Эквилибриум":alert("Ого! Мой тоже!");count=1;break;
+//             case "Зелёная миля":alert("Я устал босс...");count=1;break;
+//             case "Побег из Шоушенка":alert("Да, классный");count=1;break;
+//             case "Бегущий по лезвию":alert("Один из последних, что я посмотрел.");count=1;break;
+//             case "Гарри Поттер":alert("Ну он то всем нравится :)");count=1;break;
+//             case "Константин":alert("Фильм детства, обожаю *0*");count=1;break;
+//             default:alert("Попробуй ещё раз");
+//         }
+//     }
+//     alert("Было приятно пообщаться, удачи!")
+// }
+// FavFilm();
