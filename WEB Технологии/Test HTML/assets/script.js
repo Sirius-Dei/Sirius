1b7db9e432b5b9c63f2559bd0e47e227aea019d8
Sirius <siriusdei@mail.ru>
2023-03-23 19:56:38 +0300

    Ready

---
+++ WEB Технологии/Test HTML/assets/script.js
@@ -0,0 +1,6 @@
+function GetName()
+{
+    let name = prompt("Введите имя");
+    alert(name);
+}
+GetName();
--- WEB Технологии/Test HTML/test.php
+++ WEB Технологии/Test HTML/test.php
@@ -9,20 +9,19 @@
     <link rel="stylesheet" href="./assets/css/test.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
     <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=Playfair+Display+SC&display=swap" rel="stylesheet">
 </head>
 <body>
     <header>
+        <img class="vorona" src="./assets/img/YzDZ.gif" width="100px">
         <h1>SCANDINAVIA</h1>
     </header>
         <main>
             <a href ="https://ru.wikipedia.org/wiki/%D0%98%D1%81%D0%BB%D0%B0%D0%BD%D0%B4%D1%81%D0%BA%D0%B8%D0%B5_%D0%BC%D0%B0%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B5_%D0%B7%D0%BD%D0%B0%D0%BA%D0%B8" >
             <img class="seclogo" src="https://avatanplus.com/files/resources/original/575597939ffa1155265818c7.png" alt=""></a>
-
         </main>
         <footer>
-            
         </footer>
-    
 </body>
 </html>
